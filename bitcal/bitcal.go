8b90d860b17aecb1ffe7d2d5d3cf890f07219903
赵留超 <429281869@qq.com>
2021-04-26 14:26:24 +0800

    add bit cal

---
+++ bitcal/bitcal.go
@@ -0,0 +1,54 @@
+package bitcal
+
+func ParseByBitOrAfter(id int) string {
+	var bitArr []string = ParseByBitOrAfterArr(id)
+	if len(bitArr) == 0 {
+		return ""
+	}
+
+	return strings.Join(bitArr, ",")
+}
+
+func ParseByBitOrAfterArr(id int) []string {
+	var arr []string = make([]string, 0)
+	for i := 0; i < id; i++ {
+		tmp := pow(2, i)
+		if id&tmp == tmp {
+			arr = append(arr, strconv.Itoa(tmp))
+		}
+	}
+
+	return arr
+}
+
+func pow(x float64, n int) int {
+	if x == 0 {
+		return 0
+	}
+	result := calPow(x, n)
+	if n < 0 {
+		result = 1 / result
+	}
+
+	return int(result)
+}
+
+func calPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+
+	//向右移动一位
+	result := calPow(x, n>>1)
+	result *= result
+
+	//如果n是奇数
+	if n&1 == 1 {
+		result *= x
+	}
+
+	return result
+}
